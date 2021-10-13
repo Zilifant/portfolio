e9832513c17c0c1b01635536d49e43ed376370cd
Scott Silsbe <scott.silsbe@gmail.com>
2021-10-13 16:55:36 -0400

    fixed footer quote url bug

--- src/content/quotes.js
+++ src/content/quotes.js
@@ -4,21 +4,21 @@ export const quotes = [
   {
     text: 'Nature to be commanded must be obeyed.',
     author: 'Francis Bacon',
-    url: 'en.wikipedia.org/wiki/Francis_Bacon'
+    url: 'https://en.wikipedia.org/wiki/Francis_Bacon'
   },
   {
     text: 'Love, and do what you will.',
     author: 'Augustine',
-    url: 'en.wikipedia.org/wiki/Augustine_of_Hippo'
+    url: 'https://en.wikipedia.org/wiki/Augustine_of_Hippo'
   },
   {
     text: 'Nothing in life is as important as you think it is, while you are thinking about it.',
     author: 'Daniel Kahneman',
-    url: 'en.wikipedia.org/wiki/Thinking,_Fast_and_Slow'
+    url: 'https://en.wikipedia.org/wiki/Thinking,_Fast_and_Slow'
   },
   {
     text: 'Freedom discovers man the moment he loses concern over what impression he is making or about to make.',
     author: 'Bruce Lee',
-    url: 'en.wikiquote.org/wiki/Bruce_Lee'
+    url: 'https://en.wikiquote.org/wiki/Bruce_Lee'
   },
 ]
