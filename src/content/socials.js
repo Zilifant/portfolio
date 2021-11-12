367ead483d59840021038730bf324c3ef9afcd0a
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-12 14:12:30 -0500

    updated social media links

---
+++ src/content/socials.js
@@ -0,0 +1,20 @@
+// Social Media Data
+
+export const socials = {
+  email: {
+    id: 'personal-email',
+    url: 'mailto:scott.silsbe@gmail.com'
+  },
+  github: {
+    id: 'github',
+    url: 'https://github.com/Zilifant'
+  },
+  linkedin: {
+    id: 'linkedin',
+    url: 'https://www.linkedin.com/in/scottsilsbe'
+  },
+  twitter: {
+    id: 'twitter-personal',
+    url: 'https://twitter.com/scottsilsbe'
+  }
+}
