ac63d0af5a99e148030fe4108fd30921e772414f
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-16 18:50:51 -0500

    added app/github button links

--- src/pages/Code.svelte
+++ src/pages/Code.svelte
@@ -1,21 +1,32 @@
 <!-- Code -->
 
 <script>
-
   export let isCurrent;
   $: current = isCurrent ? 'current' : '';
 
 </script>
 
 <div class={`page-wrapper code ${current}`}>
-  <a
-    class='mhk-url'
+  <section class='grd'>
+    <div class='project-title mhk'>
+      <h1>MHK</h1>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+    </div>
+    <a
+      class='url-pseudo-btn app-url mhk'
+      href='https://mhk.vercel.app/'
+      target="_blank" rel="noopener noreferrer"
+    >
+      MHK Live
+    </a>
+    <a
+    class='url-pseudo-btn github-url mhk'
     href='https://mhk.vercel.app/'
     target="_blank" rel="noopener noreferrer"
-  >
-    MHK
-  </a>
-  <section class='content'><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></section>
+    >
+      MHK Github
+    </a>
+  </section>
 </div>
 
 <style lang="scss">
