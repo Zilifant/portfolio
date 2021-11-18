709212637a33fee1496ea774b3b74e24c2da0068
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-18 16:46:46 -0500

    added quotes

--- src/content/quotes.js
+++ src/content/quotes.js
@@ -11,14 +11,14 @@ export const quotes = [
     author: 'Augustine',
     url: 'https://en.wikipedia.org/wiki/Augustine_of_Hippo'
   },
-  // {
-  //   text: 'Nothing in life is as important as you think it is, while you are thinking about it.',
-  //   author: 'Daniel Kahneman',
-  //   url: 'https://en.wikipedia.org/wiki/Thinking,_Fast_and_Slow'
-  // },
-  // {
-  //   text: 'Freedom discovers man the moment he loses concern over what impression he is making or about to make.',
-  //   author: 'Bruce Lee',
-  //   url: 'https://en.wikiquote.org/wiki/Bruce_Lee'
-  // },
+  {
+    text: 'Nothing in life is as important as you think it is, while you are thinking about it.',
+    author: 'Daniel Kahneman',
+    url: 'https://en.wikipedia.org/wiki/Thinking,_Fast_and_Slow'
+  },
+  {
+    text: 'Freedom discovers man the moment he loses concern over what impression he is making or about to make.',
+    author: 'Bruce Lee',
+    url: 'https://en.wikiquote.org/wiki/Bruce_Lee'
+  },
 ]
