914fc5c561e5c67123f2f4a39d178c7919a09966
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-23 15:12:01 -0500

    added resume content and structure

--- src/pages/Resume.svelte
+++ src/pages/Resume.svelte
@@ -6,10 +6,9 @@
   export let isCurrent;
   $: current = isCurrent ? 'current' : '';
 
-  //-----------------//
-  //Collapsible Lists//
-  //-----------------//
+  const exp = res.experience;
 
+  // Collapsible Lists
   function expand() {
     this.classList.toggle("rotated");
     let content = this.nextElementSibling;
@@ -20,46 +19,84 @@
     };
   };
 
+  function renderYears(start, end) {
+    if (!end || (start === end)) return start;
+    return `${start}-${end}`;
+  }
+
 </script>
 
 <div class={`page-wrapper resume ${current}`}>
-  <section class="content">
-    <p class='resume-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  </section>
-  
-  <section class="content">
-    <div class="exl-wrapper">
-      <div
-        class="exl-btn"
-        type="button"
-        on:click|preventDefault={expand}
-      >
-        <div class="exl-btn-icon-wrap">
-          <div class="exl-btn-icon-fore preload"></div>
-          <div class="exl-btn-icon-back"></div>
+  <section class="content res-intro">
+    <div class='achievements'>
+      {#each res.intro.achievements as achieve}
+        <p class='resume-text res-achieve'>{achieve}</p>
+      {/each}
+    </div>
+    <div class='endorsements'>
+      {#each res.intro.endorsements as {name, title, quote}}
+        <div class='endorsement'>
+          <p class='resume-text endorse-quote'>{quote}</p>
+          <p class='resume-text endorse-person'>- {name}, {title}</p>
         </div>
-        <div class="exl-btn-text">
-          <div class="exl-btn-text-head">
-            <h6>Position</h6>
-            <h6>2018-2020</h6>
-          </div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-      </div>
-      <div class="exl-content">
-        <ul class="exl-content-ul">
-          <li class="exl-content-li">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-          <li class="exl-content-li">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-          <li class="exl-content-li">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-          <li class="exl-content-li">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-          <li class="exl-content-li">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-          <li class="exl-content-li">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
-        </ul>
-      </div>
+      {/each}
     </div>
   </section>
-  
-  <section class="content">
-    <p class='resume-text'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+  <section class="content res-skills">
+    <ul>skills
+      {#each res.skills as skill}
+        <p class='resume-text res-skill'>{skill}</p>
+      {/each}
+    </ul>
+  </section>
+
+  <section class="content res-edu">
+    <ul>education
+      {#each res.education as {degree, subject, name, year}}
+        <p class='resume-text edu-degree'>{degree}</p>
+        <p class='resume-text edu-subject'>{subject}</p>
+        <p class='resume-text edu-name'>{name}</p>
+        <p class='resume-text edu-year'>{year}</p>
+      {/each}
+    </ul>
+  </section>
+
+  {#each res.experience as exp}
+    <section class='content res-exp'>
+      <div class="exl-wrapper">
+        <div
+          class="exl-btn"
+          type="button"
+          on:click|preventDefault={expand}
+        >
+          <div class="exl-btn-icon-wrap">
+            <div class="exl-btn-icon-fore preload"></div>
+            <div class="exl-btn-icon-back"></div>
+          </div>
+          <div class="exl-btn-text">
+            <div class="exl-btn-text-head">
+              <h6>{exp.title}</h6>
+              <h6>{renderYears(exp.startYear, exp.endYear)}</h6>
+            </div>
+            <p>{exp.organization}</p>
+            <p>{exp.summary}</p>
+          </div>
+        </div>
+        <div class="exl-content">
+          <ul class="exl-content-ul">
+            {#each exp.bullets as {lead, text}}
+              <li class="exl-content-li"><span class='lead'>{lead}:</span> {text}</li>
+            {/each}
+          </ul>
+        </div>
+      </div>
+    </section>
+  {/each}
+
+  <section class="content res-interests">
+    {#each res.interests as interest}
+      <p class='resume-text res-interest'>{interest}</p>
+    {/each}
   </section>
 </div>
