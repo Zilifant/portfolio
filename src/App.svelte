1ff4f45d569777d7f7e4aa09fb5e36f42a713f11
Scott Silsbe <scott.silsbe@gmail.com>
2021-11-29 16:19:32 -0500

    version 0.7.1

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
     flyDirection
   } from './utilities';
 
-  const version = '0.7.0';
+  const version = '0.7.1';
 
   onMount(() => {
     const toggle = document.querySelector('.theme-switch-checkbox');
