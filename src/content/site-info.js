9324fea63c5648f3dbface61972b778b3f67eec3
Scott Silsbe <scott.silsbe@gmail.com>
2021-12-31 16:13:52 -0500

    added pos(ition) prop to pages array

--- src/content/site-info.js
+++ src/content/site-info.js
@@ -3,10 +3,10 @@
 export const version = '0.7.7';
 
 export const pages = [
-  { id: 'bio', color: 'pink' },
-  { id: 'resume', color: 'blue' },
-  { id: 'code', color: 'green' },
-  { id: 'writing', color: 'orange' },
+  { id: 'bio',     color: 'pink',   pos: 1 },
+  { id: 'resume',  color: 'blue',   pos: 2 },
+  { id: 'code',    color: 'green',  pos: 3 },
+  { id: 'writing', color: 'orange', pos: 4 },
 ];
 
 export const bio  = 'bio';
