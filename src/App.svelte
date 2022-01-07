562fe3c42b0d680ef10df2f761c450d6ba551762
Scott Silsbe <scott.silsbe@gmail.com>
2022-01-06 21:21:08 -0500

    break out Navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -3,53 +3,29 @@
 <script>
   import Wrap from './utility/Wrap.svelte';
   import Header from './utility/Header.svelte';
+  import Navbar from './utility/Navbar.svelte';
   import Footer from './utility/Footer.svelte';
   import Bio from './pages/Bio.svelte';
   import Resume from './pages/Resume.svelte';
   import Writing from './pages/Writing.svelte';
   import Code from './pages/Code.svelte';
-  import { version, pages, bio, res, code, writ, dark,
+  import {
+    version, bio, res, code, writ, dark,
   } from './content/site-info';
   import {
-    getRandomQuote,
-    setFlyDirection,
-    getInitialPageId,
-    removePreloadClass,
+    getRandomQuote, getInitialPageId, removePreloadClass,
   } from './utilities';
 
   removePreloadClass({ firstLoad: true });
 
-  $: isCurrentPage = (pg) => (pg === page) ? 'current' : '';
-  $: isPrevPage = (pg) => (pg === prevPage) ? 'prev' : 'not-prev';
   $: pageData = (page) => (!!page) ? page : bio;
   $: themeData = (theme) => (!!theme) ? theme : dark;
 
-  let prevPage, flyTo;
-  let dropNavState = 'hidden';
+  let flyTo = 'left';
   let page = getInitialPageId();
   let theme = localStorage.getItem('theme');
   let quote = getRandomQuote();
 
-  function switchPage(newPage) {
-    // If link to current page triggered, do nothing.
-    if (newPage === page) return;
-
-    // Update state.
-    prevPage = page;
-    page = newPage;
-    flyTo = setFlyDirection(prevPage, page);
-    quote = getRandomQuote();
-
-    removePreloadClass({ firstLoad: false });
-
-    // If viewing dropNav, minimize it.
-    if (document.documentElement.clientWidth <= 600) toggleDropNav();
-  };
-
-  function toggleDropNav() {
-    if (dropNavState === 'hidden') return dropNavState = 'visible';
-    dropNavState = 'hidden';
-  };
 </script>
 
 <main
@@ -60,37 +36,11 @@
 
   <Header bind:theme={theme} />
 
-  <nav class='main-nav'>
-    {#each pages as { id }}
-      <a
-        class={`main-nav-item mni-${id} ${isPrevPage(id)} ${isCurrentPage(id)}`}
-        href={`#${id}`}
-        on:click={() => switchPage(id)}
-      >{id}</a>
-    {/each}
-    <div class='drop-nav'>
-      <button
-        class='drop-nav-btn'
-        on:click|preventDefault={toggleDropNav}
-      >
-        <div class='drop-nav-btn-icon'>
-          <div class='drop-nav-btn-icon-bar'></div>
-          <div class='drop-nav-btn-icon-bar'></div>
-          <div class='drop-nav-btn-icon-bar'></div>
-        </div>
-        <div class='drop-nav-title'>Scott Silsbe</div>
-      </button>
-      <div class={`drop-nav-items ${dropNavState}`}>
-        {#each pages as { id }}
-          <a
-            class={`drop-nav-item ${isPrevPage(id)} ${isCurrentPage(id)}`}
-            href={`#${id}`}
-            on:click={() => switchPage(id)}
-          ><div>{id}</div></a>
-        {/each}
-      </div>
-    </div>
-  </nav>
+  <Navbar
+    bind:page={page}
+    bind:quote={quote}
+    bind:flyTo={flyTo}
+  />
 
   <div class='content-wrapper'>
     <div class='transition-grid'>
---
+++ src/utility/Navbar.svelte
@@ -0,0 +1,71 @@
+<!-- Navbar -->
+
+<script>
+
+  import { pages } from '../content/site-info';
+  import {
+    setFlyDirection, getRandomQuote, removePreloadClass
+  } from '../utilities';
+
+  export let page, quote, flyTo;
+
+  let prevPage;
+  let dropNavState = 'hidden';
+
+  $: isCurrentPage = (pg) => (pg === page) ? 'current' : '';
+  $: isPrevPage = (pg) => (pg === prevPage) ? 'prev' : 'not-prev';
+
+  function switchPage(newPage) {
+    // If link to current page triggered, do nothing.
+    if (newPage === page) return;
+
+    // Update state.
+    prevPage = page;
+    page = newPage;
+    flyTo = setFlyDirection(prevPage, page);
+    quote = getRandomQuote();
+
+    removePreloadClass({ firstLoad: false });
+
+    // If viewing dropNav, minimize it.
+    if (document.documentElement.clientWidth <= 600) toggleDropNav();
+  };
+
+  function toggleDropNav() {
+    if (dropNavState === 'hidden') return dropNavState = 'visible';
+    dropNavState = 'hidden';
+  };
+
+</script>
+
+<nav class='main-nav'>
+  {#each pages as { id }}
+    <a
+      class={`main-nav-item mni-${id} ${isPrevPage(id)} ${isCurrentPage(id)}`}
+      href={`#${id}`}
+      on:click={() => switchPage(id)}
+    >{id}</a>
+  {/each}
+  <div class='drop-nav'>
+    <button
+      class='drop-nav-btn'
+      on:click|preventDefault={toggleDropNav}
+    >
+      <div class='drop-nav-btn-icon'>
+        <div class='drop-nav-btn-icon-bar'></div>
+        <div class='drop-nav-btn-icon-bar'></div>
+        <div class='drop-nav-btn-icon-bar'></div>
+      </div>
+      <div class='drop-nav-title'>Scott Silsbe</div>
+    </button>
+    <div class={`drop-nav-items ${dropNavState}`}>
+      {#each pages as { id }}
+        <a
+          class={`drop-nav-item ${isPrevPage(id)} ${isCurrentPage(id)}`}
+          href={`#${id}`}
+          on:click={() => switchPage(id)}
+        ><div>{id}</div></a>
+      {/each}
+    </div>
+  </div>
+</nav>
