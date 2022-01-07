c9613be586a6332fa5c13dee85bf21f5a772c019
Scott Silsbe <scott.silsbe@gmail.com>
2022-01-07 11:14:56 -0500

    site-info.js => constants.js

--- src/utilities.js
+++ src/utilities.js
@@ -1,7 +1,7 @@
 // Utilities
 
 import quotes from './content/quotes.json';
-import { pageIds } from './content/site-info';
+import { pageIds } from './content/constants';
 
 //------------//
 //Random Quote//
