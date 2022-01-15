a482c6a8a6778d3c7508aae080ce504da4f13acf
Scott Silsbe <scott.silsbe@gmail.com>
2022-01-15 15:16:40 -0500

    added Loading component

---
+++ src/components/utility/Loading.svelte
@@ -0,0 +1,35 @@
+<!-- Loading Overlay -->
+<!--
+Displayed in main content section while waiting for page data to load from content server.
+-->
+
+<script>
+  import { fade } from 'svelte/transition';
+
+  const delayIn = 0;
+  const durationIn = 0;
+  const delayOut = 0;
+  const durationOut = 200;
+</script>
+
+<div
+  class='transition-wrapper'
+  in:fade= '{{ delay: delayIn,  duration: durationIn  }}'
+  out:fade='{{ delay: delayOut, duration: durationOut }}'
+>
+  <div class='page-wrapper loading-grid'>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</div>
+
+<style lang='scss'>
+  @import '../../style/utility/loading.scss';
+</style>
