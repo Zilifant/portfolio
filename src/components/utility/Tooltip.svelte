223b3c6af7795dce9878b9bd7c951ae52b65834f
Scott Silsbe <scott.silsbe@gmail.com>
2022-01-25 17:03:00 -0500

    added tooltip component

---
+++ src/components/utility/Tooltip.svelte
@@ -0,0 +1,16 @@
+<!-- Tooltip -->
+
+<script>
+  export let cls, side;
+</script>
+
+<div class={`ttip-parent ${cls}`}>
+  <slot name='hoverContent'></slot>
+  <div class={`ttip ${side}`}>
+    <slot name='tipContent'></slot>
+  </div>
+</div>
+
+<style lang='scss'>
+  @import '../../style/utility/tooltip.scss';
+</style>
